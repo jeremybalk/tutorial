602b9b086104339472c42105869c460d32ac3c79
jeremy balk <jeremy.balk@hotmail.com>
2014-05-21 13:13:57 +0200

    second tutorial

---
+++ Tutorial2/Assets/Scripts/Lazer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lazer : MonoBehaviour {
+
+	private Transform MyTransform;
+	
+	public int LazerSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+		MyTransform = transform;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Make the lazer shoot out and go up
+
+		//gameobject = lazer = shoot-up
+
+		MyTransform.Translate(Vector3.up * LazerSpeed);
+
+
+		if (MyTransform.position.y > 5) 
+		{
+		
+		}
+}
+}
---
+++ Tutorial2/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Transform MyTransform;
+
+	public int PlayerSpeed = 5;
+	// Use this for initialization
+	void Start () {
+
+		MyTransform = transform;
+		//spawn point		 x	y	z
+		// position to be at -3 -3 -1
+
+		MyTransform.position = new Vector3(-3, -3 -1);
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//move player left and right
+		MyTransform.Translate (Vector3.right * PlayerSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+		//move player up and down
+		MyTransform.Translate (Vector3.up * PlayerSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+		// world borders
+	
+		if(MyTransform.position.x > 16)
+		{
+			MyTransform.position = new Vector3(16, MyTransform.position.y, MyTransform.position.z);
+		}
+		else if(MyTransform.position.x < -16)
+		{
+			MyTransform.position = new Vector3(-16, MyTransform.position.y, MyTransform.position.z);
+		}
+
+		if(MyTransform.position.y > 5)
+		{
+			MyTransform.position = new Vector3(MyTransform.position.x, 5, MyTransform.position.z);
+		}
+		else if(MyTransform.position.y < -5)
+		{
+			MyTransform.position = new Vector3(MyTransform.position.x, -5, MyTransform.position.z);
+		}
+
+		//Fire the lazerzzzzz!!!!!
+		//If the player presses the spacebar, the lazerzzzzz!!!!! will fire
+
+
+	}
+}
